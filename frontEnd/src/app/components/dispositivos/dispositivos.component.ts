0e013c63fb6cd5568804f8f2f4bfc3219f083afb
Leonelcast <leonelcas.izq@gmail.com>
2021-03-10 14:28:04 -0600

    aactualizando datos

--- frontEnd/src/app/components/dispositivos/dispositivos.component.ts
+++ frontEnd/src/app/components/dispositivos/dispositivos.component.ts
@@ -7,6 +7,7 @@ import { Dispositivo } from '../../models/dispositivos'
   selector: 'app-dispositivos',
   templateUrl: './dispositivos.component.html',
   styleUrls: ['./dispositivos.component.css']
+  
 })
 export class DispositivosComponent implements OnInit {
 
