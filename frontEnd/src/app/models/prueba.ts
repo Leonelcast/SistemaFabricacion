21a7f5b1a2c6b50e5970d3e296fbb04c1f0b2dd0
EvelynDubon99 <dubon181014@unis.edu.gt>
2021-03-22 12:57:39 -0600

    completo

--- frontEnd/src/app/models/prueba.ts
+++ frontEnd/src/app/models/prueba.ts
@@ -1,6 +1,18 @@
-  
 export interface Prueba {
-   
-    _id: string,
-    content: string
-    }
+
+    id_dispositivo: number,
+    numero_serie: number,
+    nombre_marca: string,
+    nombret: string,
+    descripcion: string,
+    existencia: number,
+    precio_lista: number,
+    codigo_modelo: string,
+    ram: number,
+    almacenamiento: number,
+    procesador:string,
+    numero_cores: number,
+    color: string
+
+
+}
