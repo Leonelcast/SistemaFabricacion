181850c6b1316fd9f68ec453b236a7ebca6de802
Leonelcast <leonelcas.izq@gmail.com>
2021-03-21 21:14:20 -0600

    api sistema de ventas

--- frontEnd/src/app/services/prueba.service.ts
+++ frontEnd/src/app/services/prueba.service.ts
@@ -11,15 +11,25 @@ export class PruebaService {
   prueba: Prueba[] = [];
 
   selectedPrueba: Prueba={
-    _id:'',
-    content:''
-
+    id_dispositivo: 0,
+    numero_serie: 0,
+    nombre_marca: '',
+    nombret: '',
+    descripcion: '',
+    existencia: 0,
+    precio_lista: 0,
+    codigo_modelo: '',
+    ram: 0,
+    almacenamiento: 0,
+    procesador:'',
+    numero_cores: 0,
+    color: ''
   };
   constructor( private http: HttpClient,
     private router: Router) { 
     
   }
-  URL_API = 'http://localhost:8081/greeting';
+  URL_API = 'http://localhost:8081/data';
 
 
   getPrueba(){
