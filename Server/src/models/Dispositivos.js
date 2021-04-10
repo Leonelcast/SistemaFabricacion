84dde36bec711285f9e761fcb9bbad8157e6d6bc
Leonelcast <leonelcas.izq@gmail.com>
2021-04-10 10:23:37 -0600

    Actualizando JSON dispositivos

--- Server/src/models/Dispositivos.js
+++ Server/src/models/Dispositivos.js
@@ -1,13 +1,17 @@
 const {Schema, model} = require('mongoose')
 
 const dispositivoSchema = new Schema({
-numeroSerie:{type: String, require: true},
-modelo:{type: String, require: true},
-marca: {type: String, require: true},
-tipo: {type: String, require: true},
-memoria: {type: String, require: true},
-procesador: {type: String, require: true},
-resolucion:{type: String, require: true},
-precio:{type: Number, require: true},
+    color:{type: String, require: true},
+    descripcion:{type: String, require: true},
+    modelo:{type: String, require: true},
+    codigo_modelo:{type:String, require: true},
+    existencia:{type:Number, require: true},
+    precio:{type: Number, require: true},
+    ram:{type:Number, require: true},
+    almacenamiento:{type:Number, require:true},
+    procesador: {type: String, require: true},
+    numero_cores:{type:Number, require: true},
+    marca: {type: String, require: true},
+
 });
 module.exports = model("Dispositivos", dispositivoSchema);
