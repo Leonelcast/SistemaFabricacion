29361a97128ecf3c0ec4744eff8f1ad8c8e0106d
EvelynDubon99 <dubon181014@unis.edu.gt>
2021-04-11 16:09:50 -0600

    pedido

--- Server/src/models/Pedidos.js
+++ Server/src/models/Pedidos.js
@@ -4,7 +4,7 @@ const pedidoSchema = new Schema({
     cantidad: {type: Number, require: true},
     total: {type: Number, require: true},
     fecha_p: {type: String, require: true, default: Date.now},
-    fecha_e: {type: String, require: true},
+    fecha_e: {type: Date, require: true, default: Date.now() + + 7*24*60*60*1000},
     estado:{type: String, requiere: true, default:'Procesando Orden'},
     dispositivo:{
         type: Schema.Types.ObjectId,
